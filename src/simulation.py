905a2cdf3ceb5a1bdb5ee700c243afb607babe59
Simon Prudhomme <31334481+simonprudhomme@users.noreply.github.com>
2023-11-15 22:03:30 -0500

    add simulation and variables

--- src/simulation.py
+++ src/simulation.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from typing import List, Optional
 
+import matplotlib.pyplot as plt
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from loguru import logger
@@ -41,5 +42,64 @@ class Simulation:
             "net_worth": net_worth_results,
         }
 
-    def get_results(self):
+    def get_results_json(self):
         return self.simulation_result
+
+    def get_results_dataframe(self, save_to_excel=False):
+        # Your JSON data
+        data = self.get_results_json()
+
+        # Convert to DataFrame
+        cashflow_data = {}
+        net_worth_data = {}
+
+        for date, info in data.items():
+            cashflow_data[date] = info["cashflow"]
+            net_worth_data[date] = info["net_worth"]
+
+        cashflow_df = pd.DataFrame(cashflow_data).T
+        net_worth_df = pd.DataFrame(net_worth_data).T
+
+        # Convert index to datetime
+        cashflow_df.index = pd.to_datetime(cashflow_df.index)
+        net_worth_df.index = pd.to_datetime(net_worth_df.index)
+
+        if save_to_excel:
+            # Save the DataFrames to an Excel files
+            cashflow_output_file = "results/cashflow_results.xlsx"
+            cashflow_df.to_excel(cashflow_output_file)
+            print(f"Results saved to {cashflow_output_file}")
+
+            net_worth_output_file = "results/networth_results.xlsx"
+            net_worth_df.to_excel(net_worth_output_file)
+            print(f"Results saved to {net_worth_output_file}")
+
+        return cashflow_df, net_worth_df
+
+    def plot_results(self):
+        cashflow_df, net_worth_df = self.get_results_dataframe()
+
+        cashflow_df["Total Cash Flow"] = cashflow_df.sum(axis=1)
+        net_worth_df["Total Net Worth"] = net_worth_df.sum(axis=1)
+
+        # Plot Cash Flow Data
+        plt.figure(figsize=(15, 8))
+        plt.plot(
+            cashflow_df.index, cashflow_df["Total Cash Flow"], label="Total Cash Flow"
+        )
+        plt.xlabel("Date")
+        plt.ylabel("Amount")
+        plt.title("Cash Flow Over Time")
+
+        # Plot Net Worth Data
+        plt.figure(figsize=(15, 8))
+        plt.plot(
+            net_worth_df.index,
+            net_worth_df["Total Net Worth"],
+            label="Total Net Worth",
+            color="green",
+        )
+        plt.ylim(bottom=0)
+        plt.xlabel("Date")
+        plt.ylabel("Amount")
+        plt.title("Net Worth Over Time")
